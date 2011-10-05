d88273860ab1a16661fdebe0c84e9a0e7ee12336
Guillaume Aubert <guillaume.aubert@gmail.com>
2011-10-05 14:57:19 +0000

    add MoinMoin macro HTML.py

---
+++ HTML.py
@@ -0,0 +1,14 @@
+"""
+    HTML macro
+
+    by Christian Bird <chris.bird@lineo.com>
+    modified to be compatible with version 1.6 and onwards by <guillaume.aubert@gmail.com>
+    
+
+"""
+#DESCRIPTION: Outputs the html code contained within parens. <<HTML(<b>bold</b>)>>
+
+def macro_HTML(macro, args):
+
+    return args or " "
+
